openjdk/panama-foreign

8255903: Enable multi-register return values for native invokers
Reviewed-by: mcimadamore

src/jdk.incubator.foreign/share/classes/jdk/internal/foreign/abi/CallingSequenceBuilder.java
@@ -26,6 +26,10 @@ package jdk.internal.foreign.abi;
 
 import jdk.incubator.foreign.FunctionDescriptor;
 import jdk.incubator.foreign.MemoryLayout;
+import jdk.incubator.foreign.MemorySegment;
+import jdk.incubator.foreign.NativeSymbol;
+import jdk.incubator.foreign.ValueLayout;
+import jdk.internal.foreign.Utils;
 import sun.security.action.GetPropertyAction;
 
 import java.lang.invoke.MethodType;
@@ -42,6 +46,8 @@ public class CallingSequenceBuilder {
     private static final boolean VERIFY_BINDINGS = Boolean.parseBoolean(
             GetPropertyAction.privilegedGetProperty("jdk.incubator.foreign.VERIFY_BINDINGS", "true"));
 
+    private final ABIDescriptor abi;
+
     private boolean isTrivial;
     private final boolean forUpcall;
     private final List<List<Binding>> inputBindings = new ArrayList<>();
@@ -50,17 +56,22 @@ public class CallingSequenceBuilder {
     private MethodType mt = MethodType.methodType(void.class);
     private FunctionDescriptor desc = FunctionDescriptor.ofVoid();
 
-    public CallingSequenceBuilder(boolean forUpcall) {
+    public CallingSequenceBuilder(ABIDescriptor abi, boolean forUpcall) {
+        this.abi = abi;
         this.forUpcall = forUpcall;
     }
 
     public final CallingSequenceBuilder addArgumentBindings(Class<?> carrier, MemoryLayout layout,
                                                             List<Binding> bindings) {
-        verifyBindings(true, carrier, bindings);
-        inputBindings.add(bindings);
-        mt = mt.appendParameterTypes(carrier);
-        desc = desc.withAppendedArgumentLayouts(layout);
+        addArgumentBinding(inputBindings.size(), carrier, layout, bindings);
         return this;
+    }
+
+    private void addArgumentBinding(int index, Class<?> carrier, MemoryLayout layout, List<Binding> bindings) {
+        verifyBindings(true, carrier, bindings);
+        inputBindings.add(index, bindings);
+        mt = mt.insertParameterTypes(index, carrier);
+        desc = desc.insertArgumentLayouts(index, layout);
     }
 
     public CallingSequenceBuilder setReturnBindings(Class<?> carrier, MemoryLayout layout,
@@ -77,8 +88,55 @@ public class CallingSequenceBuilder {
         return this;
     }
 
+    private boolean needsReturnBuffer() {
+        return outputBindings.stream()
+            .filter(Binding.Move.class::isInstance)
+            .count() > 1;
+    }
+
     public CallingSequence build() {
-        return new CallingSequence(mt, desc, isTrivial, inputBindings, outputBindings);
+        boolean needsReturnBuffer = needsReturnBuffer();
+        long returnBufferSize = needsReturnBuffer ? computeReturnBuferSize() : 0;
+        long allocationSize = computeAllocationSize() + returnBufferSize;
+        if (!forUpcall) {
+            addArgumentBinding(0, NativeSymbol.class, ValueLayout.ADDRESS, List.of(
+                Binding.unboxAddress(NativeSymbol.class),
+                Binding.vmStore(abi.targetAddrStorage(), long.class)));
+            if (needsReturnBuffer) {
+                addArgumentBinding(0, MemorySegment.class, ValueLayout.ADDRESS, List.of(
+                    Binding.unboxAddress(MemorySegment.class),
+                    Binding.vmStore(abi.retBufAddrStorage(), long.class)));
+            }
+        }
+        return new CallingSequence(mt, desc, isTrivial, needsReturnBuffer, returnBufferSize, allocationSize, inputBindings, outputBindings);
+    }
+
+    private long computeAllocationSize() {
+        // FIXME: > 16 bytes alignment might need extra space since the
+        // starting address of the allocator might be un-aligned.
+        long size = 0;
+        for (List<Binding> bindings : inputBindings) {
+            for (Binding b : bindings) {
+                if (b instanceof Binding.Copy copy) {
+                    size = Utils.alignUp(size, copy.alignment());
+                    size += copy.size();
+                } else if (b instanceof Binding.Allocate allocate) {
+                    size = Utils.alignUp(size, allocate.alignment());
+                    size += allocate.size();
+                }
+            }
+        }
+        return size;
+    }
+
+    private long computeReturnBuferSize() {
+        return outputBindings.stream()
+                .filter(Binding.Move.class::isInstance)
+                .map(Binding.Move.class::cast)
+                .map(Binding.Move::storage)
+                .map(VMStorage::type)
+                .mapToLong(abi.arch::typeSize)
+                .sum();
     }
 
     private void verifyBindings(boolean forArguments, Class<?> carrier, List<Binding> bindings) {

src/jdk.incubator.foreign/share/classes/jdk/internal/foreign/abi/SharedUtils.java
@@ -296,27 +296,6 @@ public class SharedUtils {
         throw new IllegalArgumentException("String too large");
     }
 
-    static long bufferCopySize(CallingSequence callingSequence) {
-        // FIXME: > 16 bytes alignment might need extra space since the
-        // starting address of the allocator might be un-aligned.
-        long size = 0;
-        for (int i = 0; i < callingSequence.argumentCount(); i++) {
-            List<Binding> bindings = callingSequence.argumentBindings(i);
-            for (Binding b : bindings) {
-                if (b instanceof Binding.Copy) {
-                    Binding.Copy c = (Binding.Copy) b;
-                    size = Utils.alignUp(size, c.alignment());
-                    size += c.size();
-                } else if (b instanceof Binding.Allocate) {
-                    Binding.Allocate c = (Binding.Allocate) b;
-                    size = Utils.alignUp(size, c.alignment());
-                    size += c.size();
-                }
-            }
-        }
-        return size;
-    }
-
     static Map<VMStorage, Integer> indexMap(Binding.Move[] moves) {
         return IntStream.range(0, moves.length)
                         .boxed()
@@ -333,7 +312,8 @@ public class SharedUtils {
         }
         MethodType newType = oldType.dropParameterTypes(destIndex, destIndex + 1);
         int[] reorder = new int[oldType.parameterCount()];
-        assert destIndex > sourceIndex;
+        if (destIndex < sourceIndex)
+            sourceIndex--;
         for (int i = 0, index = 0; i < reorder.length; i++) {
             if (i != destIndex) {
                 reorder[i] = index++;
@@ -371,7 +351,7 @@ public class SharedUtils {
     }
 
     static MethodHandle wrapWithAllocator(MethodHandle specializedHandle,
-                                          int allocatorPos, long bufferCopySize,
+                                          int allocatorPos, long allocationSize,
                                           boolean upcall) {
         // insert try-finally to close the NativeScope used for Binding.Copy
         MethodHandle closer;
@@ -393,18 +373,17 @@ public class SharedUtils {
             insertPos = 2;
         }
 
-        // downcalls get the leading NativeSymbol/SegmentAllocator param as well
+        // downcalls get the leading SegmentAllocator param as well
         if (!upcall) {
-            closer = collectArguments(closer, insertPos++, reachabilityFenceHandle(NativeSymbol.class));
-            closer = dropArguments(closer, insertPos++, SegmentAllocator.class); // (Throwable, V?, NativeSymbol, SegmentAllocator) -> V/void
+            closer = dropArguments(closer, insertPos++, SegmentAllocator.class); // (Throwable, V?, SegmentAllocator, NativeSymbol) -> V/void
         }
 
-        closer = collectArguments(closer, insertPos++, MH_CLOSE_CONTEXT); // (Throwable, V?, NativeSymbol?, BindingContext) -> V/void
+        closer = collectArguments(closer, insertPos, MH_CLOSE_CONTEXT); // (Throwable, V?, SegmentAllocator?, BindingContext) -> V/void
 
         MethodHandle contextFactory;
 
-        if (bufferCopySize > 0) {
-            contextFactory = MethodHandles.insertArguments(MH_MAKE_CONTEXT_BOUNDED_ALLOCATOR, 0, bufferCopySize);
+        if (allocationSize > 0) {
+            contextFactory = MethodHandles.insertArguments(MH_MAKE_CONTEXT_BOUNDED_ALLOCATOR, 0, allocationSize);
         } else if (upcall) {
             contextFactory = MH_MAKE_CONTEXT_NO_ALLOCATOR;
         } else {
@@ -556,6 +535,14 @@ public class SharedUtils {
         if (exceptions != null && exceptions.length != 0) {
             throw new IllegalArgumentException("Target handle may throw exceptions: " + Arrays.toString(exceptions));
         }
+    }
+
+    public static MethodHandle maybeInsertAllocator(MethodHandle handle) {
+        if (!handle.type().returnType().equals(MemorySegment.class)) {
+            // not returning segment, just insert a throwing allocator
+            handle = insertArguments(handle, 1, THROWING_ALLOCATOR);
+        }
+        return handle;
     }
 
     // lazy init MH_ALLOC and MH_FREE handles

xiejf2020/jdk

src/java.base/share/classes/jdk/internal/foreign/abi/CallingSequenceBuilder.java
@@ -24,10 +24,14 @@
  */
 package jdk.internal.foreign.abi;
 
-import java.lang.foreign.FunctionDescriptor;
-import java.lang.foreign.MemoryLayout;
+import jdk.internal.foreign.Utils;
 import sun.security.action.GetPropertyAction;
 
+import java.lang.foreign.Addressable;
+import java.lang.foreign.FunctionDescriptor;
+import java.lang.foreign.MemoryLayout;
+import java.lang.foreign.MemorySegment;
+import java.lang.foreign.ValueLayout;
 import java.lang.invoke.MethodType;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
@@ -42,6 +46,8 @@ public class CallingSequenceBuilder {
     private static final boolean VERIFY_BINDINGS = Boolean.parseBoolean(
             GetPropertyAction.privilegedGetProperty("java.lang.foreign.VERIFY_BINDINGS", "true"));
 
+    private final ABIDescriptor abi;
+
     private boolean isTrivial;
     private final boolean forUpcall;
     private final List<List<Binding>> inputBindings = new ArrayList<>();
@@ -50,17 +56,22 @@ public class CallingSequenceBuilder {
     private MethodType mt = MethodType.methodType(void.class);
     private FunctionDescriptor desc = FunctionDescriptor.ofVoid();
 
-    public CallingSequenceBuilder(boolean forUpcall) {
+    public CallingSequenceBuilder(ABIDescriptor abi, boolean forUpcall) {
+        this.abi = abi;
         this.forUpcall = forUpcall;
     }
 
     public final CallingSequenceBuilder addArgumentBindings(Class<?> carrier, MemoryLayout layout,
                                                             List<Binding> bindings) {
-        verifyBindings(true, carrier, bindings);
-        inputBindings.add(bindings);
-        mt = mt.appendParameterTypes(carrier);
-        desc = desc.appendArgumentLayouts(layout);
+        addArgumentBinding(inputBindings.size(), carrier, layout, bindings);
         return this;
+    }
+
+    private void addArgumentBinding(int index, Class<?> carrier, MemoryLayout layout, List<Binding> bindings) {
+        verifyBindings(true, carrier, bindings);
+        inputBindings.add(index, bindings);
+        mt = mt.insertParameterTypes(index, carrier);
+        desc = desc.insertArgumentLayouts(index, layout);
     }
 
     public CallingSequenceBuilder setReturnBindings(Class<?> carrier, MemoryLayout layout,
@@ -77,8 +88,55 @@ public class CallingSequenceBuilder {
         return this;
     }
 
+    private boolean needsReturnBuffer() {
+        return outputBindings.stream()
+            .filter(Binding.Move.class::isInstance)
+            .count() > 1;
+    }
+
     public CallingSequence build() {
-        return new CallingSequence(mt, desc, isTrivial, inputBindings, outputBindings);
+        boolean needsReturnBuffer = needsReturnBuffer();
+        long returnBufferSize = needsReturnBuffer ? computeReturnBuferSize() : 0;
+        long allocationSize = computeAllocationSize() + returnBufferSize;
+        if (!forUpcall) {
+            addArgumentBinding(0, Addressable.class, ValueLayout.ADDRESS, List.of(
+                Binding.unboxAddress(Addressable.class),
+                Binding.vmStore(abi.targetAddrStorage(), long.class)));
+            if (needsReturnBuffer) {
+                addArgumentBinding(0, MemorySegment.class, ValueLayout.ADDRESS, List.of(
+                    Binding.unboxAddress(MemorySegment.class),
+                    Binding.vmStore(abi.retBufAddrStorage(), long.class)));
+            }
+        }
+        return new CallingSequence(mt, desc, isTrivial, needsReturnBuffer, returnBufferSize, allocationSize, inputBindings, outputBindings);
+    }
+
+    private long computeAllocationSize() {
+        // FIXME: > 16 bytes alignment might need extra space since the
+        // starting address of the allocator might be un-aligned.
+        long size = 0;
+        for (List<Binding> bindings : inputBindings) {
+            for (Binding b : bindings) {
+                if (b instanceof Binding.Copy copy) {
+                    size = Utils.alignUp(size, copy.alignment());
+                    size += copy.size();
+                } else if (b instanceof Binding.Allocate allocate) {
+                    size = Utils.alignUp(size, allocate.alignment());
+                    size += allocate.size();
+                }
+            }
+        }
+        return size;
+    }
+
+    private long computeReturnBuferSize() {
+        return outputBindings.stream()
+                .filter(Binding.Move.class::isInstance)
+                .map(Binding.Move.class::cast)
+                .map(Binding.Move::storage)
+                .map(VMStorage::type)
+                .mapToLong(abi.arch::typeSize)
+                .sum();
     }
 
     private void verifyBindings(boolean forArguments, Class<?> carrier, List<Binding> bindings) {

src/java.base/share/classes/jdk/internal/foreign/abi/SharedUtils.java
@@ -294,27 +294,6 @@ public class SharedUtils {
         throw new IllegalArgumentException("String too large");
     }
 
-    static long bufferCopySize(CallingSequence callingSequence) {
-        // FIXME: > 16 bytes alignment might need extra space since the
-        // starting address of the allocator might be un-aligned.
-        long size = 0;
-        for (int i = 0; i < callingSequence.argumentCount(); i++) {
-            List<Binding> bindings = callingSequence.argumentBindings(i);
-            for (Binding b : bindings) {
-                if (b instanceof Binding.Copy) {
-                    Binding.Copy c = (Binding.Copy) b;
-                    size = Utils.alignUp(size, c.alignment());
-                    size += c.size();
-                } else if (b instanceof Binding.Allocate) {
-                    Binding.Allocate c = (Binding.Allocate) b;
-                    size = Utils.alignUp(size, c.alignment());
-                    size += c.size();
-                }
-            }
-        }
-        return size;
-    }
-
     static Map<VMStorage, Integer> indexMap(Binding.Move[] moves) {
         return IntStream.range(0, moves.length)
                         .boxed()
@@ -331,7 +310,8 @@ public class SharedUtils {
         }
         MethodType newType = oldType.dropParameterTypes(destIndex, destIndex + 1);
         int[] reorder = new int[oldType.parameterCount()];
-        assert destIndex > sourceIndex;
+        if (destIndex < sourceIndex)
+            sourceIndex--;
         for (int i = 0, index = 0; i < reorder.length; i++) {
             if (i != destIndex) {
                 reorder[i] = index++;
@@ -369,7 +349,7 @@ public class SharedUtils {
     }
 
     static MethodHandle wrapWithAllocator(MethodHandle specializedHandle,
-                                          int allocatorPos, long bufferCopySize,
+                                          int allocatorPos, long allocationSize,
                                           boolean upcall) {
         // insert try-finally to close the NativeScope used for Binding.Copy
         MethodHandle closer;
@@ -391,18 +371,17 @@ public class SharedUtils {
             insertPos = 2;
         }
 
-        // downcalls get the leading NativeSymbol/SegmentAllocator param as well
+        // downcalls get the leading SegmentAllocator param as well
         if (!upcall) {
-            closer = collectArguments(closer, insertPos++, reachabilityFenceHandle(Addressable.class));
-            closer = dropArguments(closer, insertPos++, SegmentAllocator.class); // (Throwable, V?, NativeSymbol, SegmentAllocator) -> V/void
+            closer = dropArguments(closer, insertPos++, SegmentAllocator.class); // (Throwable, V?, SegmentAllocator, Addressable) -> V/void
         }
 
-        closer = collectArguments(closer, insertPos++, MH_CLOSE_CONTEXT); // (Throwable, V?, NativeSymbol?, BindingContext) -> V/void
+        closer = collectArguments(closer, insertPos, MH_CLOSE_CONTEXT); // (Throwable, V?, SegmentAllocator?, BindingContext) -> V/void
 
         MethodHandle contextFactory;
 
-        if (bufferCopySize > 0) {
-            contextFactory = MethodHandles.insertArguments(MH_MAKE_CONTEXT_BOUNDED_ALLOCATOR, 0, bufferCopySize);
+        if (allocationSize > 0) {
+            contextFactory = MethodHandles.insertArguments(MH_MAKE_CONTEXT_BOUNDED_ALLOCATOR, 0, allocationSize);
         } else if (upcall) {
             contextFactory = MH_MAKE_CONTEXT_NO_ALLOCATOR;
         } else {
@@ -554,6 +533,14 @@ public class SharedUtils {
         if (exceptions != null && exceptions.length != 0) {
             throw new IllegalArgumentException("Target handle may throw exceptions: " + Arrays.toString(exceptions));
         }
+    }
+
+    public static MethodHandle maybeInsertAllocator(MethodHandle handle) {
+        if (!handle.type().returnType().equals(MemorySegment.class)) {
+            // not returning segment, just insert a throwing allocator
+            handle = insertArguments(handle, 1, THROWING_ALLOCATOR);
+        }
+        return handle;
     }
 
     public static void checkSymbol(Addressable symbol) {